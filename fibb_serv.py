7f31d110729c2d6a1936647c6c02d0684215b5f1
ka-r-ol <karol.lemanski@wp.pl>
2021-02-06 18:32:07 +0100

    add process_time to response

--- fibb_serv.py
+++ fibb_serv.py
@@ -1,30 +1,36 @@
 from aiohttp import web
+import time
 
 
 async def help(request):
-    return web.Response(text="""
+    return web.Response(
+        text="""
        Go to Hell
        ... or use:   /fib/{fib_index} \n
-       """)
+       """
+    )
 
 
 async def fib(request):
-    n = int(request.match_info.get('n', -1))
+    n = int(request.match_info.get("n", -1))
+    start = time.time()
     if n == -1:
         return web.Response(text=f"Hot to hell or provide fib_index")
     a, b = 0, 1
     for i in range(0, n):
         a, b = b, a + b
     # return web.Response(text=f"{a}")
-    data = {'desc': 'fibonacci number',
-            'index': n,
-            'value': a
-            }
+    data = {
+        "desc": "fibonacci number",
+        "index": n,
+        "process_time": time.time() - start,
+        "value": a,
+    }
     return web.json_response(data)
 
-app = web.Application()
-app.add_routes([web.get(r'/', help),
-                web.get(r'/fib/{n:\d+}', fib)])
 
-if __name__ == '__main__':
+app = web.Application()
+app.add_routes([web.get(r"/", help), web.get(r"/fib/{n:\d+}", fib)])
+
+if __name__ == "__main__":
     web.run_app(app)
